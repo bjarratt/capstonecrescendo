360e614d0ec7fe9cef215906cce16a211f1741f6
red.ox.rxns <red.ox.rxns@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-03-06 03:38:49 +0000

    I would say this is a skeleton, but it's really more like a pile of bones.  More on this later.

---
+++ trunk/Crescendo-Server/CrescendoServer/src/logging/LogManager.java
@@ -0,0 +1,34 @@
+package logging;
+
+/**
+ * <code>LogManager</code>
+ * Encapsulates the log file for the server.  It is responsible for reading and writing
+ * logs including client connects and disconnects, errors, and player actions in and out of games.  
+ * 
+ * This implementation follows the singleton design pattern for ensuring a global and easily accessed
+ * manager.
+ * 
+ * @author red.ox.rxns@gmail.com
+ *
+ */
+final public class LogManager 
+{
+	/**
+	 * Method for accessing the LogManager
+	 * @return an object of type LogManager
+	 */
+	public static LogManager getInstance()
+	{
+		return instance;
+	}
+	
+	/* Implement read/write and other utility methods here */
+	
+	// Keep constructor private
+	private LogManager() {}
+	
+	// The LogManager will need to be available from program start up; therefore,
+	// we do not use lazy creation.  This method also withstands a multithreaded 
+	// environment.
+	private static LogManager instance = new LogManager();
+}
