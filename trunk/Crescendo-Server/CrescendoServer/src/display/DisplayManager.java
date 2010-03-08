01d55c9739d85b578640ce29a69a0230f29da73f
red.ox.rxns <red.ox.rxns@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-03-08 11:54:36 +0000

    LogManager is finished, and display manager has a JFrame with a button that does nothing.  Getting some sleep, and then starting on OODSS.

--- trunk/Crescendo-Server/CrescendoServer/src/display/DisplayManager.java
+++ trunk/Crescendo-Server/CrescendoServer/src/display/DisplayManager.java
@@ -1,5 +1,36 @@
 package display;
 
-public class DisplayManager {
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import logging.LogManager;
+
+
+public class DisplayManager 
+{
+	public DisplayManager()
+	{
+		mainFrame.setLayout(new GridLayout(3,3));
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		initializeComponents();
+	}
+	
+	public void Run()
+	{
+		mainFrame.setVisible(true);
+	}
+
+	private void initializeComponents()
+	{
+		mainFrame.setMinimumSize(new Dimension(600, 400));
+		mainFrame.setLocationByPlatform(true);
+		mainFrame.getContentPane().add(new JButton("Button"));
+	}
+	
+	private JFrame mainFrame = new JFrame();
 }
