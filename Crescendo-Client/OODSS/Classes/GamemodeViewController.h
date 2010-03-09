eb0b1ad974cce1535df9d3996e9fba18f3f24373
kaster.brandon <kaster.brandon@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-03-09 23:06:24 +0000

    Added OODSS Library

---
+++ Crescendo-Client/OODSS/Classes/GamemodeViewController.h
@@ -0,0 +1,18 @@
+//
+//  GamemodeViewController.h
+//  Crescendo
+//
+//  Created by Brandon on 3/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GamemodeViewController : UIViewController {
+
+}
+
+- (IBAction) goBack;
+
+@end
