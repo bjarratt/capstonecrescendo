7ccb8841e464d9c5600f9da3688579d5cf61753c
travis.kosarek <travis.kosarek@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-03-10 10:37:44 +0000

    i hope this commit works, im really tired and angry.

---
+++ Crescendo-Client/Travis' Updated Copy of OODSS/Classes/CrescendoAppDelegate.m
@@ -0,0 +1,90 @@
+//
+//  CrescendoAppDelegate.m
+//  Crescendo
+//
+//  Created by Brandon on 3/5/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import "CrescendoAppDelegate.h"
+#import "CrescendoViewController.h"
+
+@implementation CrescendoAppDelegate
+
+@synthesize window;
+@synthesize viewController;
+@synthesize client;
+
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {    
+    /*
+	 * Get an instance of the ChatTranslations scope.
+	 */
+	TranslationScope* scope = [ChatTranslations get];
+    
+	/*
+	 * Initialize the client with the ChatTranslations scope.
+	 */
+	self.client = [[XMLClient alloc] initWithHostAddress:@"192.168.11.5" andPort:2108 
+									 andTranslationScope:scope];
+	
+	/*
+	 * Designate self as the client's delegate.
+	 */
+	self.client.delegate = self;
+	
+	/*
+	 * Set the ChatRoomViewController's client as the client.
+	 */
+	viewController.client = self.client;
+	
+	/*
+	 * Set up the chatRoomViewController as the CHAT_UPDATE_DELEGATE in the application scope.
+	 
+	[self.client.scope setObject:[NSValue valueWithPointer:(chatRoomViewController)] forKey:CHAT_UPDATE_DELEGATE];
+	*/
+	
+	/*
+	 * Connect the client to the server.
+	 */
+	[self.client connect];	
+	
+    // Override point for customization after app launch    
+    [window addSubview:viewController.view];
+    [window makeKeyAndVisible];
+}
+
+
+- (void)dealloc {
+    [viewController release];
+    [window release];
+    [super dealloc];
+}
+
+/*
+ * XMLClientDelegate methods:
+ */
+- (void) connectionTerminated:(XMLClient*)client
+{
+	NSLog(@"The connection terminated!\n");
+	
+}
+
+- (void) connectionAttemptFailed:(XMLClient*) connection
+{
+	NSLog(@"The connection failed to connect!\n");
+}
+
+- (void) connectionSuccessful:(XMLClient*) client withSessionId:(NSString*) sessionId;
+{
+	NSLog(@"Connection successful with session id:%@\n", sessionId);
+}
+
+/*
+ * Close the connection when the user hits the home button.
+ */
+- (void) applicationWillTerminate:(UIApplication*) application
+{
+	[client disconnect];
+}
+@end
