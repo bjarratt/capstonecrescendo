71b7860ea6a0c0ccf74f0bc903ccf26725b9882a
kaster.brandon <kaster.brandon@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-03-15 22:10:11 +0000

    This is the first functional prototype project without all the OODSS overload of unneccessary junk.

---
+++ trunk/Crescendo-Client/Crescendo/Classes/CrescendoAppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  CrescendoAppDelegate.h
+//  Crescendo
+//
+//  Created by Brandon on 3/5/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "XMLClient.h"
+#import "ChatTranslations.h"
+
+@class CrescendoViewController;
+
+@interface CrescendoAppDelegate : NSObject <UIApplicationDelegate, XMLClientDelegate> {
+    UIWindow *window;
+    CrescendoViewController *viewController;
+	XMLClient* client;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet CrescendoViewController *viewController;
+@property (nonatomic, retain) XMLClient *client;
+
+@end
+
