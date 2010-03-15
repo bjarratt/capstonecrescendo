fd23978724f5e5e0ca78b70dc8da646ca436262f
kaster.brandon <kaster.brandon@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-03-15 22:01:53 +0000

    Crescendo client without all the OODSS junk inside. All functioning aspects from our first prototype installation.

---
+++ trunk/Crescendo-Client/Crescendo/Classes/HelpViewController.h
@@ -0,0 +1,18 @@
+//
+//  HelpViewController.h
+//  Crescendo
+//
+//  Created by Brandon on 3/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HelpViewController : UIViewController {
+
+}
+
+- (IBAction) goBack;
+
+@end
