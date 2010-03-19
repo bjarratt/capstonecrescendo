a6f74dfab605c9f011100517c24ecdbbfcdc48b5
kaster.brandon <kaster.brandon@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-03-19 08:40:21 +0000

    Deallocate resources

--- trunk/Crescendo-Client/Crescendo/TrainingView.m
+++ trunk/Crescendo-Client/Crescendo/TrainingView.m
@@ -2,8 +2,10 @@
 
 @implementation TrainingView
 
+#pragma mark UIScrollViewDelegate methods
+
 - (void) scrollViewDidEndDragging: (UIScrollView *) scrollView willDecelerate: (BOOL) decelerate {
-	if (decelerate == YES)
+	if (decelerate == YES) // Wait until it is done decelerating (calls scrollViewDidEndDecelerating)
 		return;
 	
 	[self determineScrollViewPage:scrollView];
@@ -12,6 +14,8 @@
 - (void) scrollViewDidEndDecelerating: (UIScrollView *) scrollView {
 	[self determineScrollViewPage:scrollView];
 }
+
+#pragma mark Determine Scroll Page
 
 - (void) determineScrollViewPage: (UIScrollView *) scrollView {
 	float page = scrollView.contentOffset.x / scrollView.contentSize.width;
@@ -37,12 +41,21 @@
 	}
 }
 
+#pragma mark Initialize ScrollView 
+
 - (IBAction) gotoScrollView {
     myLengthScrollView.contentSize = CGSizeMake(885, 200);
 	myPitchScrollView.contentSize = CGSizeMake(2395, 200);
 	[myLengthScrollView setDelegate:self];
 	[myPitchScrollView setDelegate:self];
 	[self addSubview:myHolderView];
+}
+
+- (void) dealloc {
+	[myHolderView release];
+	[myPitchScrollView release];
+	[myLengthScrollView release];
+    [super dealloc];
 }
 
 @end
