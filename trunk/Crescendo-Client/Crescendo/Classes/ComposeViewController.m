591f68422de4a0f7c409cf1ded8f5ac8103bd378
kaster.brandon <kaster.brandon@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-03-25 10:22:49 +0000

    Dynamic pitch scroll added. Added auto-update note and pitch methods with corresponding label. Updated page determination method.

--- trunk/Crescendo-Client/Crescendo/Classes/ComposeViewController.m
+++ trunk/Crescendo-Client/Crescendo/Classes/ComposeViewController.m
@@ -15,6 +15,8 @@
 @synthesize myLengthScrollView;
 @synthesize myPitchScrollView;
 @synthesize buildLabel;
+@synthesize noteLength;
+@synthesize notePitch;
 
 #pragma mark UIScrollViewDelegate Methods
 
@@ -36,92 +38,94 @@
 	
 	if (scrollView == myLengthScrollView)
 	{
-		if (page >= 0.47)
+		if (page >= 0.503)
 		{
-			lengthPage = 4;
+			noteLength = @"eighthnote";
 		}
-		else if(page >= 0.285)
+		else if(page >= 0.2997)
 		{
-			lengthPage = 3;
+			noteLength = @"quarternote";
 		}
-		else if (page >= 0.1)
+		else if (page >= 0.0958)
 		{
-			lengthPage = 2;
+			noteLength = @"halfnote";
 		}
 		else if (page >= 0)
 		{
-			lengthPage = 1;
+			noteLength = @"wholenote";
 		}
 	}
 	else if (scrollView == myPitchScrollView)
 	{
-		if (page >= 0.87)
+		if (page >= 0.8469)
 		{
-			pitchPage = 15;
+			notePitch = @"E5";
 		}
-		else if (page >= 0.86)
+		else if (page >= 0.7840)
 		{
-			pitchPage = 14;
+			notePitch = @"FSharp5";
 		}
-		else if (page >= 0.84)
+		else if (page >= 0.7211)
 		{
-			pitchPage = 13;
+			notePitch = @"F5";
 		}
-		else if (page >= 0.78)
+		else if (page >= 0.6583)
 		{
-			pitchPage = 12;
+			notePitch = @"GSharp5";
 		}
-		else if (page >= 0.73)
+		else if (page >= 0.5954)
 		{
-			pitchPage = 11;
+			notePitch = @"G5";
 		}
-		else if (page >= 0.66)
+		else if (page >= 0.5325)
 		{
-			pitchPage = 10;
+			notePitch = @"ASharp5";
 		}
-		else if (page >= 0.59)
+		else if (page >= 0.4696)
 		{
-			pitchPage = 9;
+			notePitch = @"A5";
 		}
-		else if (page >= 0.52)
+		else if (page >= 0.4068)
 		{
-			pitchPage = 8;
+			notePitch = @"B5";
 		}
-		else if (page >= 0.45)
+		else if (page >= 0.3439)
 		{
-			pitchPage = 7;
+			notePitch = @"CSharp6";
 		}
-		else if (page >= 0.38)
+		else if (page >= 0.2810)
 		{
-			pitchPage = 6;
+			notePitch = @"C6";
 		}
-		else if (page >= 0.31)
+		else if (page >= 0.2181)
 		{
-			pitchPage = 5;
+			notePitch = @"DSharp6";
 		}
-		else if (page >= 0.24)
+		else if (page >= 0.1553)
 		{
-			pitchPage = 4;
+			notePitch = @"D6";
 		}
-		else if (page >= 0.17)
+		else if (page >= 0.0924)
 		{
-			pitchPage = 3;
+			notePitch = @"E6";
 		}
-		else if (page >= 0.1)
+		else if (page >= 0.0295)
 		{
-			pitchPage = 2;
+			notePitch = @"FSharp6";
 		}
 		else if (page >= 0)
 		{
-			pitchPage = 1;
+			notePitch = @"F6";
 		}
 	}
+	
+	[self build];
 }
 
 #pragma mark Interface Methods
 
 - (IBAction) build {
-	buildLabel.text = [NSString stringWithFormat:@"%d / %d", lengthPage, pitchPage];
+	buildLabel.text = [NSString stringWithFormat:@"%@ / %@", noteLength, notePitch];
 }
 
 - (IBAction) goBack {
@@ -133,16 +137,25 @@
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
-	pitchPage = 1;
-	lengthPage = 1;
+	/************************
+	 * Initialize Variables *
+	 ************************/
+	noteLength = @"wholenote";
+	notePitch = @"F6";
+	
+	/******************
+	 * User Interface *
+	 ******************/
+	
+	float xCoord = 0;
 	
 	/*
 	 * Note Length Scrollview
 	 */
-	float xCoord = 76;
+	xCoord = 76;
 	myLengthScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 65, 320, 200)];
     myLengthScrollView.delegate = self;
-	myLengthScrollView.contentSize = CGSizeMake(xCoord + 168*4 + 76, 200);
+	myLengthScrollView.contentSize = CGSizeMake(xCoord + 168*4 + xCoord, 200);
 	myLengthScrollView.scrollEnabled = YES;
 	myLengthScrollView.canCancelContentTouches = NO;
 	[myLengthScrollView setUserInteractionEnabled:YES];
@@ -179,18 +192,134 @@
 	
 	/*
 	xCoord += 168;
-	UIImageView * = [[UIImageView alloc] initWithFrame: imageRect];
+	UIImageView * = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
 	[ setImage:[UIImage imageNamed: @".png"]];
 	.opaque = YES;
-	[myLengthScrollView addSubview:];
+	[myPitchScrollView addSubview:];
 	[ release];
 	*/
 	
-
 	[self.view addSubview:myLengthScrollView];
 	
-	myPitchScrollView.contentSize = CGSizeMake(2715, 200);
-	[myPitchScrollView setDelegate:self];
+	/*
+	 * Note Pitch Scrollview
+	 */
+	xCoord = 76;
+	myPitchScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 265, 320, 200)];
+    myPitchScrollView.delegate = self;
+	myPitchScrollView.contentSize = CGSizeMake(xCoord + 168*15 + xCoord, 200);
+	myPitchScrollView.scrollEnabled = YES;
+	myPitchScrollView.canCancelContentTouches = NO;
+	[myPitchScrollView setUserInteractionEnabled:YES];
+	
+	/*
+	 * Note Pitches
+	 */
+	UIImageView *pitF6 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitF6 setImage:[UIImage imageNamed: @"F6.png"]];
+	pitF6.opaque = YES;
+	[myPitchScrollView addSubview:pitF6];
+	[pitF6 release];
+	
+	xCoord += 168;
+	UIImageView *pitFSharp6 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitFSharp6 setImage:[UIImage imageNamed: @"FSharp6.png"]];
+	pitFSharp6.opaque = YES;
+	[myPitchScrollView addSubview:pitFSharp6];
+	[pitFSharp6 release];
+	
+	xCoord += 168;
+	UIImageView *pitE6 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitE6 setImage:[UIImage imageNamed: @"E6.png"]];
+	pitE6.opaque = YES;
+	[myPitchScrollView addSubview:pitE6];
+	[pitE6 release];
+	
+	xCoord += 168;
+	UIImageView *pitD6 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitD6 setImage:[UIImage imageNamed: @"D6.png"]];
+	pitD6.opaque = YES;
+	[myPitchScrollView addSubview:pitD6];
+	[pitD6 release];
+	
+	xCoord += 168;
+	UIImageView *pitDSharp6 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitDSharp6 setImage:[UIImage imageNamed: @"DSharp6.png"]];
+	pitDSharp6.opaque = YES;
+	[myPitchScrollView addSubview:pitDSharp6];
+	[pitDSharp6 release];
+	
+	xCoord += 168;
+	UIImageView *pitC6 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitC6 setImage:[UIImage imageNamed: @"C6.png"]];
+	pitC6.opaque = YES;
+	[myPitchScrollView addSubview:pitC6];
+	[pitC6 release];
+	
+	xCoord += 168;
+	UIImageView *pitCSharp6 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitCSharp6 setImage:[UIImage imageNamed: @"CSharp6.png"]];
+	pitCSharp6.opaque = YES;
+	[myPitchScrollView addSubview:pitCSharp6];
+	[pitCSharp6 release];
+	
+	xCoord += 168;
+	UIImageView *pitB5 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitB5 setImage:[UIImage imageNamed: @"B5.png"]];
+	pitB5.opaque = YES;
+	[myPitchScrollView addSubview:pitB5];
+	[pitB5 release];
+	
+	xCoord += 168;
+	UIImageView *pitA5 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitA5 setImage:[UIImage imageNamed: @"A5.png"]];
+	pitA5.opaque = YES;
+	[myPitchScrollView addSubview:pitA5];
+	[pitA5 release];
+	
+	xCoord += 168;
+	UIImageView *pitASharp5 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitASharp5 setImage:[UIImage imageNamed: @"ASharp5.png"]];
+	pitASharp5.opaque = YES;
+	[myPitchScrollView addSubview:pitASharp5];
+	[pitASharp5 release];
+	
+	xCoord += 168;
+	UIImageView *pitG5 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitG5 setImage:[UIImage imageNamed: @"G5.png"]];
+	pitG5.opaque = YES;
+	[myPitchScrollView addSubview:pitG5];
+	[pitG5 release];
+	
+	xCoord += 168;
+	UIImageView *pitGSharp5 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitGSharp5 setImage:[UIImage imageNamed: @"GSharp5.png"]];
+	pitGSharp5.opaque = YES;
+	[myPitchScrollView addSubview:pitGSharp5];
+	[pitGSharp5 release];
+	
+	xCoord += 168;
+	UIImageView *pitF5 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitF5 setImage:[UIImage imageNamed: @"F5.png"]];
+	pitF5.opaque = YES;
+	[myPitchScrollView addSubview:pitF5];
+	[pitF5 release];
+	
+	xCoord += 168;
+	UIImageView *pitFSharp5 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitFSharp5 setImage:[UIImage imageNamed: @"FSharp5.png"]];
+	pitFSharp5.opaque = YES;
+	[myPitchScrollView addSubview:pitFSharp5];
+	[pitFSharp5 release];
+	
+	xCoord += 168;
+	UIImageView *pitE5 = [[UIImageView alloc] initWithFrame: CGRectMake(xCoord, 0, 160, 160)];
+	[pitE5 setImage:[UIImage imageNamed: @"E5.png"]];
+	pitE5.opaque = YES;
+	[myPitchScrollView addSubview:pitE5];
+	[pitE5 release];
+	
+	[self.view addSubview:myPitchScrollView];
 }
 
 /*
