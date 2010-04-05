8ece887daff1b960fb20490d8626576f22932a76
heytherebrandon <heytherebrandon@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-04-05 08:25:57 +0000

    initial changes to display class.  draws a simple staff and player icons

--- trunk/Crescendo-Server/CrescendoServer/src/displayManager/DisplayGUI.java
+++ trunk/Crescendo-Server/CrescendoServer/src/displayManager/DisplayGUI.java
@@ -1,14 +1,50 @@
-package displayManager;
-
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
 /**
  *	The DisplayGUI class handles all of the graphical aspects of the display
  *
- *	@author Travis Kosarek
+ *	@author Brandon Jarratt
  */
-public class DisplayGUI
+public class DisplayGUI extends JPanel
 {
+	ArrayList<Note> playerNotes = new ArrayList<Note>();
+    Image img = null;
+    int offset = 0;
+
 	public DisplayGUI()
 	{
+        this.setPreferredSize(new Dimension(800,600));
+        this.invalidate();
+    }
+	
+	public void getNote(Note n)
+	{
+		playerNotes.add(n);
+		//update the screen with the new note
+	}
 
+    @Override
+    public void paintComponent(Graphics graphics)
+	{
+		Graphics2D g = (Graphics2D) graphics;
+        g.drawImage(Images.getNoteImage(img), 0, 50, this);
+        g.drawImage(Images.getNoteImage(img), 200, 50, this);
+        g.drawImage(Images.getNoteImage(img), 400, 50, this);
+        g.drawImage(Images.getNoteImage(img), 600, 50, this);
+
+        g.drawRoundRect(50, 450, 100, 100, 20, 20);
+        g.drawRoundRect(250, 450, 100, 100, 20, 20);
+        g.drawRoundRect(450, 450, 100, 100, 20, 20);
+        g.drawRoundRect(650, 450, 100, 100, 20, 20);
+    }
+    
+	public void reDraw()
+	{
+		for(int i=0; i<playerNotes.size(); i++)
+        {
+            //draw playerNotes[i] on the screen
+            //3 measures on the screen at any one time
+        }
 	}
 }
---
+++ trunk/Crescendo-Server/CrescendoServer/src/displayManager/Images.java
@@ -0,0 +1,26 @@
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+/**
+ *	The Images class is a placeholder for image reading functions
+ *
+ *	@author Brandon Jarratt
+ */
+public class Images
+{
+	public static Image getNoteImage(Image image)
+	{
+		Image img = null;
+		try
+        {
+			img = ImageIO.read(new File("staff.png"));
+		}
+        catch (IOException e)
+        {
+			e.printStackTrace();
+		}
+		return img;
+	}
+}
