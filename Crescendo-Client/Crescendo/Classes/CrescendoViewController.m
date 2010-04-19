6138531b66deb95f1f1d8afc2e741390d2582cc5
kaster.brandon <kaster.brandon@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-04-19 02:11:05 +0000

    Moved around the XMLClient. Added IP view to change IP address to server.

--- Crescendo-Client/Crescendo/Classes/CrescendoViewController.m
+++ Crescendo-Client/Crescendo/Classes/CrescendoViewController.m
@@ -19,6 +19,10 @@
 @synthesize playerId;
 @synthesize gamemodeViewController;
 @synthesize helpViewController;
+@synthesize gameModes;
+@synthesize connect;
+@synthesize ipText;
+@synthesize ipLabel;
 
 #pragma mark PlayNoteUpdateDelegate Method
 
@@ -45,8 +49,62 @@
 	[self presentModalViewController:gamemodeViewController animated:YES];
 }
 
+- (IBAction) goConnect {
+	/*
+	 * Setup Client
+	 */
+	// Get an instance of the ChatTranslations scope.
+	TranslationScope* scope = [ServerTranslations get];
+    
+	// Initialize the client with the ChatTranslations scope.
+	// 192.168.1.105
+	// 128.194.143.165
+	self.client = [[XMLClient alloc] initWithHostAddress:@"192.168.1.110" andPort:2108 
+									 andTranslationScope:scope];
+	
+	// Designate self as the client's delegate.
+	self.client.delegate = self;
+	
+	// Set up the self as the CONNECTION_UPDATE_DELEGATE in the application scope.
+	[self.client.scope setObject:[NSValue valueWithPointer:(self)] forKey:CONNECTION_UPDATE_DELEGATE];
+	
+	// Connect the client to the server.
+	[self.client connect];	
+	
+	/*
+	 * Connection Request
+	 */
+	
+	// Initialize a Connection Request.
+	ConnectionRequest* request = [[ConnectionRequest alloc] init];
+	
+	// Setup the client to send the connection request a little later in the run loop.
+	[client performSelector:@selector(sendMessage:) withObject: request];
+	
+	[request release];
+	
+	// Draw main screen with game modes
+    [self drawMain];
+}
+
 - (IBAction) goToHelpView {
 	[self presentModalViewController:helpViewController	animated:YES];
+}
+
+#pragma mark Draw Methods
+
+- (void) drawIP {
+	connect.hidden = NO;
+	ipText.hidden = NO;
+	ipLabel.hidden = NO;
+	gameModes.hidden = YES;
+}
+
+- (void) drawMain {
+	connect.hidden = YES;
+	ipText.hidden = YES;
+	ipLabel.hidden = YES;
+	gameModes.hidden = NO;
 }
 
 #pragma mark Initialize View Methods
@@ -72,14 +130,7 @@
  // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
  - (void)viewDidLoad {
 	 [super viewDidLoad];
-	 
-	 // Initialize a Connection Request.
-	 ConnectionRequest* request = [[ConnectionRequest alloc] init];
-	 
-	 // Setup the client to send the connection request a little later in the run loop.
-	 [client performSelector:@selector(sendMessage:) withObject: request];
-	 
-	 [request release];
+	 [self drawIP];
  }
 
 #pragma mark Autorotate Orientation Override
@@ -111,6 +162,31 @@
 	[gamemodeViewController release];
 	[helpViewController release];
     [super dealloc];
+}
+
+#pragma mark XMLClientDelegate Methods
+
+- (void) connectionTerminated:(XMLClient*)client
+{
+	NSLog(@"The connection terminated!\n");
+}
+
+- (void) connectionAttemptFailed:(XMLClient*) connection
+{
+	NSLog(@"The connection failed to connect!\n");
+}
+
+- (void) connectionSuccessful:(XMLClient*) client withSessionId:(NSString*) sessionId;
+{
+	NSLog(@"Connection successful with session id:%@\n", sessionId);
+}
+
+#pragma mark UIApplicationDelegate Methods
+
+// Close the connection when the user hits the home button.
+- (void) applicationWillTerminate:(UIApplication*) application
+{
+	[client disconnect];
 }
 
 @end
