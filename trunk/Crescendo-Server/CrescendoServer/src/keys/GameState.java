68c7a1280cacb87fe58ca9ce3736c23e1de2b996
travis.kosarek <travis.kosarek@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-05-01 02:23:16 +0000

    game state updated

--- trunk/Crescendo-Server/CrescendoServer/src/keys/GameState.java
+++ trunk/Crescendo-Server/CrescendoServer/src/keys/GameState.java
@@ -13,20 +13,7 @@ public class GameState
 	public final static String SPLASH_SCREEN = "splashscreen";
 	public final static String CONNECT = "connect";
 		
-	public final static String GAME_MODES = "gamemodes";
-	
-	// *******************************************************************************
-	public final static String LENGTH_TRAINING = "lengthtraining";
-	public final static String PITCH_TRAINING = "pitchtraining";
-	public final static String NOTE_TRAINING = "notetraining";
-	public final static String CONCERT_MASTER = "concertmaster";
-	public final static String MUSICAL_IPHONES = "musicaliphones";
-	public final static String NOTES_AROUND_THE_ROOM = "notesaroundtheroom";
-	public final static String COMP_TIME = "comptime";
-	public final static String COMPOSE = "compose";
-	// *******************************************************************************
-	
-	public final static String GAME_INFO = "gameinfo";
+	public final static String KEY_MASTER = "keymaster";
 	
 	public final static String GAME_OPTIONS = "gameoptions";
 	public final static String SET_TEMPO = "settempo";
@@ -39,7 +26,7 @@ public class GameState
 	public final static String PAUSE = "pause";
 	
 	public final static String POST_GAME = "postgame";
-	public final static String REVIEW = "review";
+	public final static String PLAY_SONG = "playsong";
 	
 	public final static String EXIT = "exit";
 }
