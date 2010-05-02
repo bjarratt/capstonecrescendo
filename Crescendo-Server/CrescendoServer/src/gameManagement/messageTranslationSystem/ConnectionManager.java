a85f208b6f39e5cc76211dd6cd202aac3f3bbd72
kaster.brandon <kaster.brandon@a30e578c-0c87-11df-945e-ed10c03ad965>
2010-05-02 04:30:17 +0000

    updates for CHIP

--- Crescendo-Server/CrescendoServer/src/gameManagement/messageTranslationSystem/ConnectionManager.java
+++ Crescendo-Server/CrescendoServer/src/gameManagement/messageTranslationSystem/ConnectionManager.java
@@ -12,41 +12,37 @@ public class ConnectionManager
 {
 	private GameManager gameManager;
 
-	/*****iPhone Emulators*****/
-	Player player1;
-	Player player2;
-	Player player3;
-	Player player4;
-
-	Thread p1;
-	Thread p2;
-	Thread p3;
-	Thread p4;
-	/**************************/
 
 	public ConnectionManager()
 	{
 		//set up the display to receive messages
 		gameManager = new GameManager();
 
-		/*******************Beginning of test code********************/
-		//This section emulates four connected iPhones sending random messages
-
+		/******************iPhone Emulators******************/
+		Player player1;
+		Thread p1;
 		player1 = new Player("player1",this);
-		player2 = new Player("player2",this);
-		player3 = new Player("player3",this);
-		player4 = new Player("player4",this);
-
 		p1 = new Thread(player1);
-		p2 = new Thread(player2);
-		p3 = new Thread(player3);
-		p4 = new Thread(player4);
-
 		p1.start();
-		p2.start();
-		p3.start();
-		p4.start();
-		/**********************End of test code***********************/
+//		
+//		Player player2;
+//		Thread p2;
+//		player2 = new Player("player2",this);
+//		p2 = new Thread(player2);
+//		p2.start();
+//		
+//		Player player3;
+//		Thread p3;
+//		player3 = new Player("player3",this);
+//		p3 = new Thread(player3);
+//		p3.start();
+//		
+//		Player player4;
+//		Thread p4;
+//		player4 = new Player("player4",this);
+//		p4 = new Thread(player4);
+//		p4.start();
+		/****************************************************/
 
 		gameManager.run();
 	}
